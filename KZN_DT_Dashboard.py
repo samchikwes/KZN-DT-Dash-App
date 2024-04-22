baf30255540bc737464aadd00801f4e961274f07
samchikwes <98181636+samchikwes@users.noreply.github.com>
2024-04-22 14:35:02 +0200

    Update KZN_DT_Dashboard.py

--- KZN_DT_Dashboard.py
+++ KZN_DT_Dashboard.py
@@ -441,7 +441,7 @@ def get_min_bar_chart(throughput_slider):
               Input(component_id="throughput-slider", component_property="value"))
 def get_box_plot(throughput_slider):
     filtered_df10 = df[(df['MeanUserDataRateKbps']>=throughput_slider[0]) & (df['MeanUserDataRateKbps']<=throughput_slider[1])]
-    fig11 = px.violin(filtered_df10, x='Area', y='MeanUserDataRateKbps', color='Area', box=True, points="all", hover_data=filtered_df10.columns,
+    fig11 = px.violin(filtered_df10, x='EndDataRadioBearer', y='MeanUserDataRateKbps', color='Area', box=True, points="all", hover_data=filtered_df10.columns,
                       title='Throughput Violin Plot Per Area')
     return fig11
 
